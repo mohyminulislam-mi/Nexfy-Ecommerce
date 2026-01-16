1650e09dadd37a7e6730346da050e69d71b26189
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-01-16 22:43:44 +0600

    Make Featured Products sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,15 @@ import Hero from "@/components/pages/Home/Hero";
 import Deals from "@/components/pages/Home/Deals";
 import Image from "next/image";
 import Categories from "@/components/pages/Home/Categories";
+import FeaturedProducts from "@/components/pages/Home/FeaturedProducts";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Categories />
+      <FeaturedProducts />
+      <Deals />
     </main>
   );
 }
